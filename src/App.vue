a34fb58dd345bf56709034720fb407107329dc00
zl21 <602659667@qq.com>
2022-01-21 15:05:34 +0800

    v1: 相关依赖版本运行成功

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,117 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import Color from 'color'
+
+/** -------------------- data: -------------------- */
+let color1 = ref('#409EFF')
+const predefineColors = ref([
+  '#ff4500',
+  '#ff8c00',
+  '#ffd700',
+  '#90ee90',
+  '#00ced1',
+  '#1e90ff',
+  '#c71585',
+  'rgba(255, 69, 0, 0.68)',
+  'rgb(255, 120, 0)',
+  'hsv(51, 100, 98)',
+  'hsva(120, 40, 94, 0.5)',
+  'hsl(181, 100%, 37%)',
+  'hsla(209, 100%, 56%, 0.73)',
+  '#c7158577'
+])
+/** -------------------- mounted: -------------------- */
+onMounted(() => {
+  console.log('mounted');
+})
+/** -------------------- methods: -------------------- */
+const colorPick = (val) => {
+  color1 = val
+  changeColor(val)
+}
+const changeColor = (val) => {
+  // document.documentElement 代表相关的容器如：#box
+  //js中获取--base-color的值
+  var value = getComputedStyle(document.documentElement).getPropertyValue("--base-color");
+  //js中更改--base-color的值
+  document.documentElement.style.setProperty("--base-color", val);
+  // 颜色深度处理
+  const color = Color(val)
+  const q1 = color.lighten(0.5).hex()
+  const d1 = color.darken(0.5).hex()
+  document.documentElement.style.setProperty("--base2-color", q1);
+  document.documentElement.style.setProperty("--base3-color", d1);
+  console.log('base::', val, 'q1::', q1, 'd1::', d1);
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <el-button type="primary" @click="visible = true">Button</el-button>
+
+    <div class="themeT">
+      <el-color-picker
+        v-model="color1"
+        @change="colorPick"
+        :predefine="predefineColors"
+      ></el-color-picker>
+    </div>
+    <ul class="colorFade">
+
+      <li>base</li>
+      <li>浅</li>
+      <li>深</li>
+    </ul>
+  </div>
 </template>
 
+<style>
+:root {
+  --base-color: #ccc;
+  --base2-color: #cc0;
+  --base3-color: #c0c;
+}
+</style>
+<style lang="less">
+.colorFade {
+  position: absolute;
+  right: 100px;
+  top: 300px;
+  li {
+    color: #fff;
+    margin: 20px;
+    height: 50px;
+    width: 100px;
+    border: 1px solid;
+    &:first-child {
+      background-color: var(--base-color);
+    }
+    &:nth-child(2) {
+      background-color: var(--base2-color);
+    }
+    &:nth-child(3) {
+      background-color: var(--base3-color);
+    }
+  }
+}
+.themeT {
+  z-index: 999;
+  position: fixed;
+  top: 100px;
+  right: 100px;
+  button {
+    width: 60px;
+    height: 30px;
+    border: 1px solid red;
+    background: #fff;
+    color: var(--base-color);
+  }
+}
+</style>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+// Vue.use(ElementPlus); // Vue.use is not defined ???
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
